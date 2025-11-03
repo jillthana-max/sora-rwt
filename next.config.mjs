ab25612f0439b9fa9f295e147628d6dbd50f8dc8
JIll <jillthana@gmail.com>
2025-11-04 00:03:59 +0700

    Rename next.config.mjs to next.config.js

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',         // แทนที่ next export เดิม
-  images: { unoptimized: true }, // จำเป็นสำหรับ static export
-  // trailingSlash: true,    // (ไม่บังคับ) ถ้าอยากให้ URL ลงท้ายด้วย / เสมอ
-};
-module.exports = nextConfig;
